e823f9100821bbba18caf7eb7e6ac0b90a2288da
Yujung Jang <yjung.dev@gmail.com>
2022-11-12 19:42:22 +0900

    squash merge 테스트 (#10)
    * 첫 번째 머지 테스트
    
    * 두 번째 머지 테스트
    
    * 세 번째 머지 테스트

--- src/main/java/com/codestates/TodoController.java
+++ src/main/java/com/codestates/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
 
     // Gitflow 테스트 2
+    // squash merge 테스트
     @GetMapping("/")
     public String helloWorld() {
         return "To-do Application!";
