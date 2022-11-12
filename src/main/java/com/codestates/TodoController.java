9d33177ef3d48693730983aaa0e7cc13c29ee81b
yujung-Jang <yjung.dev@gmail.com>
2022-11-12 19:47:27 +0900

    인텔리제이에서 Git 다루기

--- src/main/java/com/codestates/TodoController.java
+++ src/main/java/com/codestates/TodoController.java
@@ -8,6 +8,7 @@ public class TodoController {
 
     // Gitflow 테스트 2
     // squash merge 테스트
+    // 인텔리제이에서 Git 다루기
     @GetMapping("/")
     public String helloWorld() {
         return "To-do Application!";
