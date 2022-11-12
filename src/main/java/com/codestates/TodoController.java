977919b052179ac208170da74e41dfa785aa89bc
yujung-Jang <yjung.dev@gmail.com>
2022-11-12 19:10:48 +0900

    Gitflow 테스트

--- src/main/java/com/codestates/TodoController.java
+++ src/main/java/com/codestates/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TodoController {
 
+    // Gitflow 테스트
     @GetMapping("/")
     public String helloWorld() {
         return "To-do Application!";
